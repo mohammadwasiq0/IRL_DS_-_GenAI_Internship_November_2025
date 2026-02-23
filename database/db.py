440fdf1eb4dc2bdb6f57355940e6886b4f5ad62d
Mohammad Wasiq <118173141+mohammadwasiq0@users.noreply.github.com>
2026-02-23 15:49:36 +0530

    Delete database directory

--- database/db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from config import Config
-
-engine = create_engine(Config.DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
